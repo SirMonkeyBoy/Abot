e583f4af7b17d7a536ba3470646a290da7079937
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2024-01-02 11:52:56 +1100

    Delete commands directory

--- commands/nextrobbery.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-    data: new SlashCommandBuilder().setName("nextrobbery").setDescription('data of next robbery'),
-    run: ({ interaction, client }) => {
-        interaction.reply('Check events for the next robbery time');
-    },
-}
