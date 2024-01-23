28f159a9a38e6f603c1b9a745cbc754b10500a41
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2024-01-23 14:18:40 +1100

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -1,14 +1,14 @@
 module.exports = client => {
 
     require('dotenv').config();
-    const { welcomeChannelID, rolesID, welcomeRoleID } = process.env;
+    const { welcomeChannelID, rolesID, welcomeRole } = process.env;
 
     client.on("guildMemberAdd", member => {
 
         console.log(`${member.username}`)
 
         const message = `Welcome <@${member.id}> to the Robbery! Do not forget to get your roles from <#${rolesID}>,`
-        const message1 = `also remember to invite all your friends. <#${welcomeRoleID}>`
+        const message1 = `also remember to invite all your friends. <@&${welcomeRole}>`
 
         const channel = member.guild.channels.cache.get(welcomeChannelID);
 
